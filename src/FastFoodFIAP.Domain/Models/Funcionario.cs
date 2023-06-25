9a732ceb285a1bb66a7e08dcd0da494e3245ddb2
analuisabavati <ana_bavati@hotmail.com>
2023-06-25 16:28:43 -0300

    fix Funcionario model

--- src/FastFoodFIAP.Domain/Models/Funcionario.cs
+++ src/FastFoodFIAP.Domain/Models/Funcionario.cs
@@ -5,7 +5,6 @@ namespace FastFoodFIAP.Domain.Models
     public class Funcionario: Entity, IAggregateRoot
     {
         
-        public int FuncionarioId { get; private set; }    
         public string? Nome { get; private set; }  = "";
         public string? Matricula { get; private set; }  = "";
         public int OcupacaoId {get; private set;}
