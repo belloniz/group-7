3aa97a271d7cbf95e308f1a2ed1c4f4fe0193aa6
analuisabavati <ana_bavati@hotmail.com>
2023-06-25 16:25:59 -0300

    Adicionamento model de Funcionário e Ocupacao

---
+++ src/FastFoodFIAP.Domain/Models/Funcionario.cs
@@ -0,0 +1,23 @@
+﻿using GenericPack.Domain;
+
+namespace FastFoodFIAP.Domain.Models
+{
+    public class Funcionario: Entity, IAggregateRoot
+    {
+        
+        public int FuncionarioId { get; private set; }    
+        public string? Nome { get; private set; }  = "";
+        public string? Matricula { get; private set; }  = "";
+        public int OcupacaoId {get; private set;}
+
+        public virtual Ocupacao? OcupacaoNavegation { get; private set; }
+        
+        public Funcionario(int id, string nome, string matricula,int ocupacaoId) 
+        {
+            Id = id;
+            Nome = nome;            
+            Matricula = matricula;
+            OcupacaoId = ocupacaoId;
+        }
+    }
+}
