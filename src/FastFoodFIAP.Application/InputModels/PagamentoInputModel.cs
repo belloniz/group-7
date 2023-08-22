6470c73e193341de2772e539ed838786dbadf368
analuisabavati <ana_bavati@hotmail.com>
2023-08-21 22:45:53 -0300

    wip

--- src/FastFoodFIAP.Application/InputModels/PagamentoInputModel.cs
+++ src/FastFoodFIAP.Application/InputModels/PagamentoInputModel.cs
@@ -5,17 +5,10 @@ namespace FastFoodFIAP.Application.InputModels
 {
     public class PagmentoInputModel
     {        
-        [Required(ErrorMessage = "O Qr do pagamento é requerido.")]
-        [MinLength(3)]
-        [MaxLength(100)]
-        [DisplayName("Qr")]        
-        public string qr { get; set; } = "";
-
         [Required(ErrorMessage = "A Situação do pagamento é requerida.")]
         [MinLength(3)]
         [MaxLength(1000)]
         [DisplayName("Situação")]
         public string Situação { get; set; } = "";
-
     }
 }
--- src/FastFoodFIAP.Domain/Commands/PagamentoCommands/PagamentoCommand.cs
+++ src/FastFoodFIAP.Domain/Commands/PagamentoCommands/PagamentoCommand.cs
@@ -5,7 +5,7 @@ namespace FastFoodFIAP.Domain.Commands.PagamentoCommands{
     public class PagamentoCommands : Command
     {
         public Guid Id { get; protected set; }
-        public Guid? ClienteId {get; protected set;}
-       
+        
+    
     }
 }
