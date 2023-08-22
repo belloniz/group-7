4e5258b8203cabfa2ffd3c7c3953689ee07d7ece
analuisabavati <ana_bavati@hotmail.com>
2023-08-21 22:46:55 -0300

    wip

---
+++ src/FastFoodFIAP.Infra.Data/Mappings/SituacoesPagamentosMap.cs
@@ -0,0 +1,25 @@
+﻿using FastFoodFIAP.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace FastFoodFIAP.Infra.Data.Mappings
+{
+    public class SituacoesPagamentosMap: IEntityTypeConfiguration<SituacaoPagamento>
+    {
+public void Configure(EntityTypeBuilder<SituacaoPagamento> builder)
+        {            
+            builder.ToTable("situacoes_pagamentos");
+   
+            builder.HasKey(c => c.Id)
+                .HasName("PRIMARY");
+
+            builder.Property(c => c.Id)
+                .HasColumnName("id");
+
+            builder.Property(c => c.Nome)
+                .HasColumnName("nome")
+                .HasMaxLength(50);               
+        }
+
+    }
+}
