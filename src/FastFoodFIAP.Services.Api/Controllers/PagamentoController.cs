84ac0462187115910d741749adfc2122975e9b43
analuisabavati <ana_bavati@hotmail.com>
2023-08-21 22:29:46 -0300

    wip

--- src/FastFoodFIAP.Services.Api/Controllers/PagamentoController.cs
+++ src/FastFoodFIAP.Services.Api/Controllers/PagamentoController.cs
@@ -2,154 +2,41 @@ using FastFoodFIAP.Application.InputModels;
 using FastFoodFIAP.Application.Interfaces;
 using FastFoodFIAP.Application.Services;
 using FastFoodFIAP.Application.ViewModels;
-using FastFoodFIAP.Domain.Models.ProdutoAggregate;
+using FastFoodFIAP.Domain.Models.Pagamento;
 using Microsoft.AspNetCore.Mvc;
 using Swashbuckle.AspNetCore.Annotations;
 
 namespace FastFoodFIAP.Services.Api.Controllers
 {
     [ApiController]
-    [Route("api/produto")]
+    [Route("api/pagamento")]
     [Produces("application/json")]
     [Consumes("application/json")]
-    public class ProdutoController : ApiController
+    public class PagamentoController : ApiController
     {
-        private readonly IProdutoApp _produtoApp;
+        private readonly IPagamentoApp _pagamentoApp;
 
-        public ProdutoController(IProdutoApp produtoApp)
+        public ProdutoController(IProdutoApp pagamentoApp)
         {
-            _produtoApp = produtoApp;
-        }
-
-        [HttpGet]
-        [SwaggerOperation(
-        Summary = "Lista todos os produtos.",
-        Description = "Lista ordenada pelo nome de todos os produtos"
-        )]
-        [SwaggerResponse(200, "Success", typeof(List<ProdutoViewModel>))]
-        [SwaggerResponse(204, "No Content")]
-        [SwaggerResponse(400, "Bad Request")]
-        [SwaggerResponse(500, "Unexpected error")]
-        public async Task<ActionResult> GetAll()
-        {
-            try
-            {
-                var lista = await _produtoApp.GetAll();
-                return CustomListResponse(lista, lista.Count);
-            }
-            catch (Exception e)
-            {
-                return Problem("Há um problema com a sua requisição - " + e.Message);
-            }
-            
-        }
-
-        [HttpGet("categoria/{id}")]
-        [SwaggerOperation(
-        Summary = "Lista todos os produtos de uma categoria.",
-        Description = "Lista ordenada pelo nome de todos os produtos de uma categoria"
-        )]
-        [SwaggerResponse(200, "Success", typeof(List<ProdutoViewModel>))]
-        [SwaggerResponse(204, "No Content")]
-        [SwaggerResponse(400, "Bad Request")]
-        [SwaggerResponse(500, "Unexpected error")]
-        public async Task<ActionResult> GetAllByCategoria([FromRoute] Guid id)
-        {
-            try
-            {
-                var lista = await _produtoApp.GetAllByCategoria(id);
-                return CustomListResponse(lista, lista.Count);
-            }
-            catch (Exception e)
-            {
-                return Problem("Há um problema com a sua requisição - " + e.Message);
-            }
-        }
-
-        [HttpGet("{id}")]
-        [SwaggerOperation(
-        Summary = "Localiza um produto.",
-        Description = "Localiza um produto pelo seu ID."
-        )]
-        [SwaggerResponse(200, "Success", typeof(ProdutoViewModel))]
-        [SwaggerResponse(400, "Bad Request")]
-        [SwaggerResponse(404, "Not Found")]
-        [SwaggerResponse(500, "Unexpected error")]
-        public async Task<IActionResult> GetById([FromRoute] Guid id)
-        {
-            try
-            {
-                return CustomResponse(await _produtoApp.GetById(id));
-            }
-            catch (Exception e)
-            {
-                return Problem("Há um problema com a sua requisição - " + e.Message);
-            }
-            
-        }
-
-        [HttpPost]
-        [SwaggerOperation(
-        Summary = "Cria um novo produto.",
-        Description = "Cria um novo produto."
-        )]
-        [SwaggerResponse(201, "Success", typeof(ProdutoInputModel))]
-        [SwaggerResponse(400, "Bad Request")]
-        [SwaggerResponse(500, "Unexpected error")]
-        public async Task<IActionResult> Post([FromBody] ProdutoInputModel produto)
-        {
-            try
-            {
-                if (!ModelState.IsValid)
-                    return CustomResponse(ModelState);
-
-                return CustomCreateResponse(await _produtoApp.Add(produto));
-            }
-            catch (Exception e)
-            {
-                return Problem("Há um problema com a sua requisição - " + e.Message);
-            }
-            
+            _pagamentoApp = pagamentoApp;
         }
 
         [HttpPut("{id}")]
         [SwaggerOperation(
-        Summary = "Atualiza um produto.",
-        Description = "Atualiza um produto."
+        Summary = "Recebe a confirmação de pagamento aprovado ou pagamento recusado",
+        Description = "Recebe a confirmação de pagamento aprovado ou pagamento recusado"
         )]
         [SwaggerResponse(204, "Success")]
         [SwaggerResponse(400, "Bad Request")]
         [SwaggerResponse(500, "Unexpected error")]
-        public async Task<IActionResult> Put([FromRoute] Guid id, [FromBody] ProdutoInputModel produto)
+        public async Task<IActionResult> Put([FromRoute] Guid id, [FromBody] PagamentoInputModel pagamento)
         {
             try
             {
                 if (!ModelState.IsValid)
                     return CustomResponse(ModelState);
 
-                return CustomNoContentResponse(await _produtoApp.Update(id, produto));
-            }
-            catch (Exception e)
-            {
-                return Problem("Há um problema com a sua requisição - " + e.Message);
-            }
-            
-        }
-
-        [HttpDelete("{id}")]
-        [SwaggerOperation(
-        Summary = "Exclui um produto.",
-        Description = "Exclui um produto."
-        )]
-        [SwaggerResponse(204, "Success")]
-        [SwaggerResponse(400, "Bad Request")]
-        [SwaggerResponse(404, "Not Found")]
-        [SwaggerResponse(500, "Unexpected error")]
-        public async Task<IActionResult> Delete([FromRoute] Guid id)
-        {
-            try
-            {
-                return CustomNoContentResponse(await _produtoApp.Remove(id));
+                return CustomNoContentResponse(await _pagamentoApp.Update(id, pagamento));
             }
             catch (Exception e)
             {
