e3d2e584884a8451524af7c63ad9f3174fa94a51
analuisabavati <ana_bavati@hotmail.com>
2023-08-29 18:28:24 -0300

    wip

--- src/FastFoodFIAP.Application/Interfaces/IPagamentoApp.cs
+++ src/FastFoodFIAP.Application/Interfaces/IPagamentoApp.cs
@@ -4,13 +4,8 @@ using FluentValidation.Results;
 
 namespace FastFoodFIAP.Application.Interfaces
 {
-    public interface IPedidoApp: IDisposable
-    {
-        Task<List<PedidoViewModel>> GetAllBySituacao(int situacaoId);
-        Task<List<PedidoViewModel>> GetAll();
-        Task<PedidoViewModel> GetById(Guid id);
-        Task<ValidationResult> Add(PedidoInputModel model);
-        Task<ValidationResult> Update(Guid id, PedidoInputModel model);
-        Task<ValidationResult> Remove(Guid id);
+    public interface IPagamentoApp: IDisposable
+    {   
+        Task<ValidationResult> Update(Guid id, PagamentoInputModel model);
     }
 }
--- src/FastFoodFIAP.Domain/Models/Pagamento.cs
+++ src/FastFoodFIAP.Domain/Models/Pagamento.cs
@@ -1,25 +1,24 @@
+using FastFoodFIAP.Domain.Models.PedidoAggregate;
 using GenericPack.Domain;
 
 namespace FastFoodFIAP.Domain.Models
 {
-    public class Pagamento: Entity, IAggregateRoot
+    public class Pagamento : Entity, IAggregateRoot
     {
-//        public String QrCode { get; private set; } = "";
-//        public decimal Valor { get; private set; }
-//        public Guid PedidoId { get; private set; }
+        public String QrCode { get; private set; } = "";
+        public decimal Valor { get; private set; }
+        public Guid PedidoId { get; private set; }
 
-//        public Guid  { get; private set;}
+        public virtual Pedido PedidoNavegation { get; private set; }
 
-//    public virtual Pedido PedidoNavegation { get; private set; }
+        private Pagamento() { }
 
-//    private Pagamento() { }
-
-//    public Pagamento(Guid id, String qrCode, decimal valor, Guid pedidoId)
-//    {
-//        Id = id;
-//        QrCode = qrCode;
-//        Valor = valor;
-//        PedidoId = pedidoId;
-//    }
-//}
+        public Pagamento(Guid id, String qrCode, decimal valor, Guid pedidoId)
+        {
+            Id = id;
+            QrCode = qrCode;
+            Valor = valor;
+            PedidoId = pedidoId;
+        }
+    }
 }
