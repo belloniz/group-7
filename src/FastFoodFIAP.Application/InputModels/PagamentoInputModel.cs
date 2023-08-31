31bb9d3b04279fa1735c1aba941f9dda30593005
analuisabavati <ana_bavati@hotmail.com>
2023-08-31 00:10:58 -0300

    wip

--- src/FastFoodFIAP.Application/InputModels/PagamentoInputModel.cs
+++ src/FastFoodFIAP.Application/InputModels/PagamentoInputModel.cs
@@ -4,11 +4,11 @@ using System.ComponentModel.DataAnnotations;
 namespace FastFoodFIAP.Application.InputModels
 {
     public class PagamentoInputModel
-    {        
+    {
+        [Required(ErrorMessage = "O id do pagamento é requerido.")]
+        public Guid Id { get; set; }
+
         [Required(ErrorMessage = "A Situação do pagamento é requerida.")]
         public int SituacaoId { get; set; }
-
-        [Required(ErrorMessage = "O id do pagamento é requerido.")] 
-        public Guid Id { get; set; }
     }
 }
